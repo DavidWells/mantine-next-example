dc83b11611ae668392fa0b281134ee299e265016
David Wells <>
2021-05-10 10:50:30 -0700

    add theme switcher

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,36 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Head from "next/head";
 import { MantineProvider } from "@mantine/core";
+import { useTheme } from 'next-themes'
+import { ThemeProvider } from 'next-themes'
+
+function ThemePicker({ onThemeChange }) {
+  const { theme, setTheme } = useTheme('light')
+  console.log('theme', theme)
+  return (
+    <div>
+      The current theme is: {theme}
+      <button onClick={() => {
+        console.log('set light')
+        setTheme('light')
+        onThemeChange('light')
+      }}>
+        Light Mode
+      </button>
+      <button onClick={() => {
+        console.log('set dark')
+        setTheme('dark')
+        onThemeChange('dark')
+      }}>
+        Dark Mode
+      </button>
+    </div>
+  )
+}
 
 export default function App(props) {
   const { Component, pageProps } = props;
+  const [ currentTheme, setCurrentTheme ] = useState('light')
 
   useEffect(() => {
     const jssStyles = document.getElementById("mantine-ssr-styles");
@@ -11,6 +38,11 @@ export default function App(props) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
   }, []);
+
+  function onThemeChange(value) {
+    console.log('setCurrentTheme', value)
+    setCurrentTheme(value)
+  }
 
   return (
     <>
@@ -21,14 +53,17 @@ export default function App(props) {
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
       </Head>
-      <MantineProvider
-        theme={{
-          /** Put your mantine theme override here */
-          colorScheme: "light",
-        }}
-      >
-        <Component {...pageProps} />
-      </MantineProvider>
+      <ThemeProvider>
+        <MantineProvider
+          theme={{
+            /** Put your mantine theme override here */
+            colorScheme: currentTheme,
+          }}
+        >
+          <Component {...pageProps} />
+          <ThemePicker onThemeChange={onThemeChange} />
+        </MantineProvider>
+      </ThemeProvider>
     </>
   );
 }
